00ce5a466b532c764050bfcb70931b142e11ebfd
P1ccione <simone.piscozzo@gmail.com>
2023-12-11 14:28:44 +0100

    manifest 5.0

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.executeScript(tab.id, { code: 'alert("Btn clicked!");' });
+  chrome.tabs.sendMessage(tab.id, { action: "showAlert" });
 });
--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "showAlert") {
-    // In questo esempio, l'alert verrà mostrato al clic su un elemento con ID "mioPulsante"
     let btn = document.getElementById("chrome-extension-btn");
 
     if (btn) {
       btn.addEventListener("click", () => {
+        console.log("Button clicked!");
         alert("Btn clicked!");
       });
     }
