cd05b464ea22a07d753018f645acd0ce1f289c84
P1ccione <simone.piscozzo@gmail.com>
2023-12-11 16:05:10 +0100

    final chrome extension 2.0

--- content.js
+++ content.js
@@ -5,7 +5,25 @@ console.log("CONTENT");
 // Add a click event listener to the element with ID "test
 document.getElementById("test").addEventListener("click", function () {
   console.log("clicked content.js");
-  alert("Hello World!");
+
+  // Make an API request
+  fetch("https://reqres.in/api/users/2")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (confirm("Are you sure you want to get the data?")) {
+        alert("Hello World! API Response: " + JSON.stringify(data));
+      } else {
+        alert("NO DATA FOR YOU <3");
+      }
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:");
+    });
 });
 
 // Send a message to the background script on page load
