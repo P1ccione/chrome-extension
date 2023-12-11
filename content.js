296b9f863e81e2b6838e0a4b796d7085fcf445a3
P1ccione <simone.piscozzo@gmail.com>
2023-12-11 16:21:50 +0100

    minor bug fixed

--- content.js
+++ content.js
@@ -5,25 +5,24 @@ console.log("CONTENT");
 // Add a click event listener to the element with ID "test
 document.getElementById("test").addEventListener("click", function () {
   console.log("clicked content.js");
-
-  // Make an API request
-  fetch("https://reqres.in/api/users/2")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (confirm("Are you sure you want to get the data?")) {
+  if (confirm("Are you sure you want to get the data?")) {
+    // Make an API request
+    fetch("https://reqres.in/api/users/2")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
         alert("Hello World! API Response: " + JSON.stringify(data));
-      } else {
-        alert("NO DATA FOR YOU <3");
-      }
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:");
-    });
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:");
+      });
+  } else {
+    alert("NO DATA FOR YOU <3");
+  }
 });
 
 // Send a message to the background script on page load
